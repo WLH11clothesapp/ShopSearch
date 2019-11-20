d24b1d9cccfabaa82878ddfe67f7d0d91836c777
mlunceford <mark7lunceford@gmail.com>
2019-11-20 11:13:26 -0700

    routes

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -1,1 +1,16 @@
-//Landing
+import React from 'react'
+
+class Landing extends React.Component {
+    constructor(){
+        super()
+        this.state = {
+
+        }
+    }
+    render(){
+        return(
+            <div className="Landing">Landing</div>
+        )
+    };
+}
+export default Landing;
