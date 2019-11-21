1a96b107791eaed590d50abae8c4268decaa39e6
colefin8 <cole.fin8@gmail.com>
2019-11-21 15:30:16 -0700

    after mark merged

--- src/_tests_/UnitTests.test.js
+++ src/_tests_/UnitTests.test.js
@@ -1,15 +1,15 @@
-import React from "react";
-import { render, unmountComponentAtNode } from "react-dom";
-import { act } from "react-dom/test-utils";
-import renderer from "react-test-renderer";
-import { HashRouter, Link } from "react-router-dom";
+import React from 'react';
+import { render, unmountComponentAtNode } from 'react-dom';
+import { act } from 'react-dom/test-utils';
+import renderer from 'react-test-renderer';
+import { HashRouter, Link } from 'react-router-dom';
 
-const { getProducts, validate } = require("./Functions");
+const { getProducts, validate } = require('./Functions');
 
-describe("Testing get products endpoint", () => {
+describe('Testing get products endpoint', () => {
   let container = null;
   beforeEach(() => {
-    container = document.createElement("div");
+    container = document.createElement('div');
     document.body.appendChild(container);
   });
 
@@ -20,92 +20,92 @@ describe("Testing get products endpoint", () => {
   });
 
   //Cole 1
-  it("Gets Products from database", async done => {
+  it('Gets Products from database', async done => {
     let products;
     await act(async () => {
       products = await getProducts();
     });
-    expect(products[0].brand).toBe("Nordstrom");
+    expect(products[0].brand).toBe('Nordstrom');
     done();
   });
 });
 
-describe("Testing our useForm function", () => {
+describe('Testing our useForm function', () => {
   //Cole 2
-  test("makes sure validate function only accepts names longer than 3 chars", () => {
+  test('makes sure validate function only accepts names longer than 3 chars', () => {
     let errors;
     act(() => {
-      errors = validate({ name: "no" });
+      errors = validate({ name: 'no' });
     });
-    expect(errors.name).toBe("Username needs to be more than 3 characters");
+    expect(errors.name).toBe('Username needs to be more than 3 characters');
   });
   //Cole 3
-  test("makes sure our validate function only accepts properly formatted emails", () => {
+  test('makes sure our validate function only accepts properly formatted emails', () => {
     let errors;
     act(() => {
-      errors = validate({ email: "notanemail" });
+      errors = validate({ email: 'notanemail' });
     });
-    expect(errors.email).toBe("Invalid Email address");
+    expect(errors.email).toBe('Invalid Email address');
   });
   //Cole 4
-  test("makes sure our validate function only accepts passwords longer than 2 chars", () => {
+  test('makes sure our validate function only accepts passwords longer than 2 chars', () => {
     let errors;
     act(() => {
-      errors = validate({ password: "no" });
+      errors = validate({ password: 'no' });
     });
-    expect(errors.password).toBe("Password needs to be more than 2 characters");
+    expect(errors.password).toBe('Password needs to be more than 2 characters');
   });
   //Cole 5
-  test("makes sure validate function requires instagram handle to be formatted correctly", () => {
+  test('makes sure validate function requires instagram handle to be formatted correctly', () => {
     let errors;
     act(() => {
-      errors = validate({ igHandle: "handle" });
+      errors = validate({ igHandle: 'handle' });
     });
-    expect(errors.igHandle).toBe("follow format @yourHandle");
+    expect(errors.igHandle).toBe('follow format @yourHandle');
   });
 });
 
 describe("Testing our useForm function doesn't accept empty inputs", () => {
   //Braiden 1
-  test("makes sure validate function returns error on empty name input", () => {
+  test('makes sure validate function returns error on empty name input', () => {
     let errors;
     act(() => {
-      errors = validate({ name: "" });
+      errors = validate({ name: '' });
     });
-    expect(errors.name).toBe("Must enter a name");
+    expect(errors.name).toBe('Must enter a name');
   });
   //Braiden 2
-  test("makes sure our validate function returns error on empty email input", () => {
+  test('makes sure our validate function returns error on empty email input', () => {
     let errors;
     act(() => {
-      errors = validate({ email: "" });
+      errors = validate({ email: '' });
     });
-    expect(errors.email).toBe("Must enter an email");
+    expect(errors.email).toBe('Must enter an email');
   });
   //Braiden 3
-  test("makes sure our validate function returns error on empty password input", () => {
+  test('makes sure our validate function returns error on empty password input', () => {
     let errors;
     act(() => {
-      errors = validate({ password: "" });
+      errors = validate({ password: '' });
     });
-    expect(errors.password).toBe("Password is required");
+    expect(errors.password).toBe('Password is required');
   });
   //Braiden 4
-  test("makes sure our validate function returns error on empty igHandle input", () => {
+  test('makes sure our validate function returns error on empty igHandle input', () => {
     let errors;
     act(() => {
-      errors = validate({ igHandle: "" });
+      errors = validate({ igHandle: '' });
     });
-    expect(errors.igHandle).toBe("Must enter an Instagram handle");
+    expect(errors.igHandle).toBe('Must enter an Instagram handle');
   });
 });
 
-describe("Testing routing", () => {
+describe('Testing routing', () => {
   //Braiden 5
   test("make sure that '/' renders the landing page correctly", () => {
     const component = renderer.create(
       <HashRouter>
-        <Link to="/" />
+        <Link to='/' />
       </HashRouter>
     );
     let tree = component.toJSON();
@@ -115,7 +115,7 @@ describe("Testing routing", () => {
   test("make sure that '/login' renders the login page correctly", () => {
     const component = renderer.create(
       <HashRouter>
-        <Link to="/login" />
+        <Link to='/login' />
       </HashRouter>
     );
     let tree = component.toJSON();
@@ -125,7 +125,7 @@ describe("Testing routing", () => {
   test("make sure that '/register' renders the login page correctly", () => {
     const component = renderer.create(
       <HashRouter>
-        <Link to="/register" />
+        <Link to='/register' />
       </HashRouter>
     );
     let tree = component.toJSON();
@@ -135,7 +135,7 @@ describe("Testing routing", () => {
   test("make sure that '/search' renders the login page correctly", () => {
     const component = renderer.create(
       <HashRouter>
-        <Link to="/search" />
+        <Link to='/search' />
       </HashRouter>
     );
     let tree = component.toJSON();
@@ -145,7 +145,7 @@ describe("Testing routing", () => {
   test("make sure that '/userprofile' renders the login page correctly", () => {
     const component = renderer.create(
       <HashRouter>
-        <Link to="/userprofile" />
+        <Link to='/userprofile' />
       </HashRouter>
     );
     let tree = component.toJSON();
@@ -155,7 +155,7 @@ describe("Testing routing", () => {
   test("make sure that '/newpost' renders the login page correctly", () => {
     const component = renderer.create(
       <HashRouter>
-        <Link to="/newpost" />
+        <Link to='/newpost' />
       </HashRouter>
     );
     let tree = component.toJSON();
