3657c7dd98f4f54c9428878464c8915ae1715478
mlunceford <mark7lunceford@gmail.com>
2019-12-02 15:12:41 -0700

    newpost text and pushing

--- src/Components/NewPost/NewPost.js
+++ src/Components/NewPost/NewPost.js
@@ -8,6 +8,7 @@ import AddProduct from '../AddProduct/AddProduct';
 
 function NewPost(props) {
   const [url, setUrl] = React.useState('');
+  const [text, setText] = React.useState('') 
   const [productCount, setProductCount] = React.useState(1); /// used to determine the count of addProduct
   const info = [];
 
@@ -17,7 +18,10 @@ function NewPost(props) {
     /// handle changes for typing in input boxes where hook is called
     setUrl(event.target.value);
   };
-
+  const handleTextChange = event => {
+    /// handle changes for typing in input boxes where hook is called
+    setText(event.target.value)
+  };
   const grabInfo = (index, stateObj) => {
     console.log('index', index);
     console.log('stateObj', stateObj);
@@ -30,7 +34,7 @@ function NewPost(props) {
       .post('/api/post', {
         user_id: props.user_id, /// built this way so we don't have to login every time to work on this page
         image: url,
-        text: 'word'
+        text: text 
       })
       .then(res => {
         console.log(res.data);
@@ -54,6 +58,7 @@ function NewPost(props) {
         })
         .then(res => {
           console.log('res.data', res.data, j);
+          props.history.push('/userprofile')
           return res.data;
         })
         .catch(err => console.log(err));
@@ -86,6 +91,14 @@ function NewPost(props) {
           onChange={handleUrlChange}
           placeholder='Copy and paste your img URL here'
         />
+        <h5> Add Description:</h5>
+        <textarea
+            cols="50" 
+            rows="4"
+            value={text}
+            onChange={handleTextChange}
+            placeholder='Add Description of your New Post'
+        ></textarea>
         <h5> Add Products:</h5>
         {newProductsList} {/* displays addProduct * product count */}
         <div className='add-product-button-styler'>
